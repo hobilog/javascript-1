d85b5eed6e4b1d27be548c5ed4281aa213508383
hobilog <nicehack122@mail.ru>
2023-04-13 14:27:44 +0300

    Первая задача (Расчет возможности работы и ее стоимость)

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-let a = 5;
-let b = 5.6;
-console.log(typeof a);
-a = 'строка';
-console.log(typeof a);
-let isAdmin = false;
-console.log(typeof isAdmin)
-e = "dasdas"
-console.log(typeof e)
-
-let c;
-console.log(typeof c);
-c = null;
-console.log(typeof c);
-let d = null;
-console.log(typeof (d == null));
+const canWork  = 9 * 5 >= 40
+const pay = 80 * 40
+console.log('Смогу ли взяться за работу: ' + canWork)
+console.log('Сколько попрошу за оплату: ' + pay + ' $')
