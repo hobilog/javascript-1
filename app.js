7297412709671e51c4456ec1b98778409b0a19d4
hobilog <nicehack122@mail.ru>
2023-04-13 14:37:22 +0300

    Шаблонные строки

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const canWork  = 9 * 5 >= 40
-const pay = 80 * 40
-console.log('Смогу ли взяться за работу: ' + canWork)
-console.log('Сколько попрошу за оплату: ' + pay + ' $')
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
+
+const template = author + ' заказал ' + projectName + ' по цене ' + price + ' $'
+console.log(template)
+
+const template2 = `${author} заказал ${projectName} по цене ${price} $`
+console.log(template2);
+
+const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+console.log(template3)
+
+const template4 = `Проект
+Цена: ${price}$`;
+console.log(template4)
