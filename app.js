e154b59209bf2868b9b4e95ccc7ce93b47f30ff1
hobilog <nicehack122@mail.ru>
2023-04-21 00:51:17 +0300

    Упраженение капча

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const bmwX3Price = 100000;
-const fordFocusPrice = 10000;
-const budget = 200000;
+const r = Math.random();
+const seq = r > 0.5 ? '-' : '+';
+const mes = prompt(`Сколько будет 7 ${seq} 15?`);
 
-let message = budget > bmwX3Price ? 'BMW' : budget > fordFocusPrice ? 'Ford' : 'Велосипед';
-
-console.log(`Я хочу купить ${message}`);
+if (mes === 'Я не робот') {
+    console.log('Успех!');
+} else if (seq === '-' && mes === '-8') {
+    console.log('Успех!');
+} else if (seq === '+' && mes === '22') {
+    console.log('Успех!')
+} else {
+    console.log('Неудача :(');
+}
