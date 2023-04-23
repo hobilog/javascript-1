75f2839a8bb889b74c43aa6ca7786c55d602c22b
hobilog <nicehack122@mail.ru>
2023-04-23 19:37:08 +0300

    Анонимные функции

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-function logName(name, surname) {
-    console.log(`Моё имя: ${name} ${surname}`);
+function powerOfTwo(num) {
+    return num * num;
 }
 
-logName('Семён', 'Станкин');
+console.log(powerOfTwo(5));
 
-function countDepositSum(depositInUSD, month, rate) {
-    const sum = depositInUSD * (1 + rate / 12) ** month;
-    return sum;
+const poft = function (num) {
+    return num * num;
 }
-
-const example1 = countDepositSum(1000, 24, 0.12);
-console.log(example1);
-
-const example2 = countDepositSum(1000, 48, 0.10);
-console.log(example2);
+console.log(poft(6));
