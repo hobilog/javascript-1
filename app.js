e1200eebd2e861b1f819d888c179ffafc458b71f
hobilog <nicehack122@mail.ru>
2023-04-23 19:41:36 +0300

    Стрелочные функции

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function powerOfTwo(num) {
+    console.log(num);
     return num * num;
 }
 
 console.log(powerOfTwo(5));
 
-const poft = function (num) {
-    return num * num;
-}
+const poft = num => {
+    console.log(num);
+    return num*num;
+};
 console.log(poft(6));
